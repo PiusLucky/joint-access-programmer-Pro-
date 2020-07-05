abe1228dfffdcfb4f7580faa9e97e7bf5b041260
luckypius50 <luckypius50@gmail.com>
2020-07-05 11:04:31 +0100

    ~Login Excluded for specific url

--- authorize/views.py
+++ authorize/views.py
@@ -16,6 +16,15 @@ from django.template.loader import get_template
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+def login_excluded(redirect_to):
+    """ This decorator kicks authenticated users out of a view """ 
+    def _method_wrapper(view_method):
+        def _arguments_wrapper(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                return redirect(redirect_to) 
+            return view_method(request, *args, **kwargs)
+        return _arguments_wrapper
+    return _method_wrapper
 
 def my_custom_send_email(toaddr,id):
     form_instance = User
@@ -31,7 +40,7 @@ def my_custom_send_email(toaddr,id):
     message.send()
 
 
-
+@login_excluded('/')
 def login(request):
 	title = "Log-in"
 	template = "main/login.html"
